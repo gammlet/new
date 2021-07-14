6bf5ebc661f0ac23e205abf2a3c1b5af37292821
gammlet <37348223+gammlet@users.noreply.github.com>
2021-07-14 13:01:07 +0300

    Create ass.js

---
+++ ass.js
@@ -0,0 +1,23 @@
+let c = Number(prompt('первый игрк введи число'))
+
+ let b = c+1
+ alert('подойди 2 игрок')
+ let quastion = confirm("хош задачу?");
+  //let b = ++c
+ if (quastion>0){ 
+ alert('c+1='+''+ b)
+  
+  const a = prompt('чему равно с?')
+  console.log(a)
+   if ( a==c ){
+     alert('да ты просто гений')
+      
+   }
+    else{
+      alert('кхм иди в шкилу молодой')
+      console.log(false) 
+    }
+ }
+else {
+    alert("как хош")
+} 
